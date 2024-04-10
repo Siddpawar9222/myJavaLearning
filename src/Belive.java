685d4dfc8d09661de3a66cf8d798a4924c94b1d9
Siddhesh <siddpawar583@gmail.com>
2024-04-10 07:05:40 +0530

    Revision

--- src/Belive.java
+++ src/Belive.java
@@ -1,9 +1,6 @@
 public class Belive {
+
   public static void main(String[] args) {
-      Belive b = new Belive() ;
-      System.out.println(b.getClass());
-      System.out.println(b.getClass().getName());
-      System.out.println(Belive.class);
-      System.out.println(new Belive());
+     
   }
 }
